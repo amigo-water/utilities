8a8051ecbb08eead97bc11bc7a89135f4a9ccfc4
ashritha-water <ashritha.amigo@gmail.com>
2025-05-22 16:55:31 +0530

    Working copy for all services for generating build

--- services/policy-management-service/src/index.js
+++ services/policy-management-service/src/index.js
@@ -1,35 +1,33 @@
-require('dotenv').config();
 const express = require('express');
-// const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
-const sequelize = require('./config/database');
-const models = require('./models');
+const helmet = require('helmet').default;
 
 const app = express();
 
 // Middleware
-app.use(helmet());
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
-// Database connection
-
-// Sync all models with the database
-sequelize.sync({ alter: true }).then(() => {
-  console.log('Database synchronized');
+// Routes
+app.get('/', (req, res) => {
+  res.send('Policy Management Service is running');
 });
 
+// Database connection
+// Sync all models with the database
+// sequelize.sync({ alter: true }).then(() => {
+//   console.log('Database synchronized');
+// });
+
 // Error handling middleware
+// @ts-ignore
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({
-    status: 'error',
-    message: 'Something went wrong!'
-  });
+  res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+  console.log(`Policy Management Service running on port ${PORT}`);
+});
